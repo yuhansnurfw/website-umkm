6c47bf221bb9957138d9eae066a2d168f36f5bac
yuhansnurfw <fakhri.yuhans.nurfawwas25080@mhs.uingusdur.ac.id>
2026-09-07 11:10:06 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const promoButton = document.querySelector("#promoButton");
+
+promoButton.addEventListener("click", () => {
+    promoButton.textContent = "Promo: Beli 2 gratis tester!";
+    console.log("Promo Kopi Nusa berhasil ditampilkan.");
+});
