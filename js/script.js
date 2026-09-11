cf0a597d1fadd06a75c86acecf15431e6af1cf38
yuhansnurfw <fakhri.yuhans.nurfawwas25080@mhs.uingusdur.ac.id>
2026-09-11 23:18:20 +0800

    p01

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
-    promoButton.textContent = "Promo: Beli 2 gratis tester!";
-    console.log("Promo Kopi Nusa berhasil ditampilkan.");
+    promoButton.textContent = "Promo: Install Ulang Windows + Software + Game Gratis!";
+    console.log("Promo Jasa Oprek Yuhans berhasil ditampilkan.");
 });
